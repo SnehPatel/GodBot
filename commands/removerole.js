34bbe8c05cfdb6a88f06da4e177e586965f8bcac
SnehPatel <Sneh0107@gmail.com>
2020-10-14 22:11:13 -0400

    Added permissions to the roles

--- commands/removerole.js
+++ commands/removerole.js
@@ -4,10 +4,15 @@ module.exports = {
 	name: 'removerole',
 	description: 'Give role to others',
 	execute(message, args) {
-    let role = message.guild.roles.cache.find(r => r.name === args[1]);
-    let member = message.mentions.members.first();
-    member.roles.remove(role).catch(console.error);
-    console.log(member);
-		message.channel.send(role.name + " role removed from " + member.user.username + ".");
+		let role = message.guild.roles.cache.find(r => r.name === args[1]);
+		let member = message.mentions.members.first();
+		
+		if(message.member.hasPermission('ADMINISTRATOR')){
+			member.roles.remove(role).catch(console.error);
+			console.log(member);
+			message.channel.send(role.name + " role removed from " + member.user.username + ".");
+		}else{
+			message.channel.send(message.member.displayName + " you do not have permissions to do this!");
+		}
 	},
 };
