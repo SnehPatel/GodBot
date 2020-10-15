bdf2543459d4903cc4be80144033a8df5d152e69
SnehPatel <Sneh0107@gmail.com>
2020-10-14 22:16:02 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -7,7 +7,6 @@ module.exports = {
 
       **-help** - Displays the help menu.
       **-ping** - Pong.
-      **-play <youtube-url>** - Plays music.
       **-poll <question>** - Ask a poll.
       **-giverole <user> <role>** - Assigns the user the role.
       **-removerole <user> <role>** - Removes the role from the user.
