18aa9bcafe8aef3203c07707bcbfb6a7681e3591
SnehPatel <Sneh0107@gmail.com>
2020-10-23 22:48:39 -0400

    Update removerole.js

--- commands/moderation/removerole.js
+++ commands/moderation/removerole.js
@@ -8,11 +8,31 @@ module.exports = class RemoveRole extends commando.Command {
             group: 'moderation',
             memberName: 'removerole',
             description: 'Remove the specified role from the user',
-            
+            argsType: 'multiple',
+            clientPermissions: [
+                'MANAGE_ROLES'
+            ],
+            userPermissions:[
+                'MANAGE_ROLES'
+            ]
         })
     }
 
-    async run(msg) {
-        
+    async run(message, args) {
+
+        const target = message.mentions.users.first()
+        let memberUsername = message.mentions.members.first();
+        let role = message.guild.roles.cache.find(r => r.name === args[1]);
+
+        if(!target){
+            message.reply('Please specify someone to give role to.')
+            return
+        }
+
+        const {guild} = message
+        const member = guild.members.cache.get(target.id)
+
+        member.roles.remove(role).catch(console.error)
+        message.channel.send(role.name + " has been removed from " + memberUsername.user.username + ".")
     }
 }
